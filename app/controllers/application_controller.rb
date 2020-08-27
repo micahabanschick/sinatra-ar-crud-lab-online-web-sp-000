6fea4e345d6dc24bc3bbd2f807be6bca4b4ec5fc
Micah Banschick <micha.ban@gmail.com>
2020-08-27 20:30:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,47 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles/new' do #loads new form
+    erb :new
+  end
+
+  post '/articles' do  #creates a Article
+    @article = Article.create(params)
+  end 
+
+  get '/articles' do #loads index page
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do  #loads show page
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do #loads edit form
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  post '/articles/:id' do  #updates a Article
+    @article = Article.find_by_id(params[:id])
+    @article.name = params[:name]
+    @article.content = params[:content]
+    @article.save
+  end
+
+  get '/articles/:id/delete' do 
+    erb :delete
+  end
+
+
+
+  post '/articles/:id/delete' do #delete action
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+    redirect to '/articles'
+  end
+  
 end
